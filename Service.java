87e89930868edfbf498b8d35947c92df14f4b32e
Isthifa <87200953+Isthifa@users.noreply.github.com>
2023-06-22 14:50:31 +0530

    Update Service.java

--- Service.java
+++ Service.java
@@ -1,1 +1,6 @@
+  Follow followSeller(UUID userId, UUID sellerID) throws AgriTradeException;
+
+  Follow unfollowSeller(UUID userID, UUID sellerID) throws AgriTradeException;
+
+  List<Crop> getCropsByFollowedSeller(UUID userID, UUID sellerID) throws AgriTradeException;
 
